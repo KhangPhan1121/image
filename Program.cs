845700cad1cb212bd912a8b3beb48ace5f7feca6
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2024-01-02 17:01:56 +0700

    them file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Image;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; ; i++)
+        {
+            string a = Console.ReadLine();
+
+            ImageInput imageInput = new ImageInput();
+            string b = imageInput.Convert(a);
+
+            ImageDownloader imageDownloader = new ImageDownloader();
+            var c = imageDownloader.DownloadImage(b);
+
+            ImageSave imageSave = new ImageSave();
+            imageSave.SaveImageToFile(c, a + ".PNG");
+        }
+
+
+    }
+}
